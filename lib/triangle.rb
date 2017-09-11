5c6554c93fa232dcaa90550f7ef73522edf16883
Guest User <Guest@epicodus-27.local>
2017-09-11 16:17:58 -0700

    Pass rspec

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,10 +6,16 @@ class Triangle
   end
 
   def triangle?
-    if (@side1 + @side2) > @side3
-      true
+    @side1 = @side1.to_i
+    @side2 = @side2.to_i
+    @side3 = @side3.to_i
+
+    if ((@side1 + @side2) > @side3) && ((@side1 + @side3) > @side2) && ((@side2 + @side3) > @side1)
+    "Its a triangle"
+      # true
     else
-      false
+    "Its not a triangle"
+      # false
     end
   end
 end
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -4,14 +4,15 @@ require 'pry'
 
 describe(Triangle) do
   describe("#triangle?") do
-    it("returns false if it is not a triangle") do
-      test_triangle = Triangle.new(2, 2, 8)
-      expect(test_triangle.triangle?()).to(eq(false))
+    it("print false if it is not a triangle") do
+      test_triangle = Triangle.new(2, 2, 4)
+      expect(test_triangle.triangle?()).to(eq("Its not a triangle"))
     end
+    
 
     it("returns true if it is a triangle") do
       test_triangle = Triangle.new(2, 2, 2)
-      expect(test_triangle.triangle?()).to(eq(true))
+      expect(test_triangle.triangle?()).to(eq("Its a triangle"))
     end
 
   end
